700b6dda3e46ff8fc2d9626bade514d1d416ee01
Richard Higenbottam <rich_higenbottam@hotmail.com>
2022-11-11 12:57:24 +0000

    photoapp

---
+++ src/pages/About.js
@@ -0,0 +1,56 @@
+import React from "react";
+
+// import images
+import WomanImg from "../img/about/woman.png";
+// import link
+import { Link } from "react-router-dom";
+// import motion
+import { motion } from "framer-motion";
+// import transition
+import { transition1 } from "../transitions";
+
+const About = () => {
+  return (
+    <motion.section
+      initial={{ scale: 0, y: "100%" }}
+      animate={{ scale: 1, y: 0 }}
+      exit={{ scale: 0, y: "100%" }}
+      transition={transition1}
+      className="section"
+    >
+      <div className="container mx-auto h-full relative">
+        {/* Text & img wrapper */}
+        <div className="flex flex-col lg:flex-row h-full items-center justify-center gap-x-24 text-center lg:text-left lg:pt-16">
+          {/* img */}
+          <div className="flex-1 max-h-96 lg:max-h-max order-2 lg:order-none overflow-hidden">
+            <img src={WomanImg} alt="" />
+          </div>
+          {/* text */}
+          <motion.div
+            initial={{ scale: 0, y: "-80%" }}
+            animate={{ scale: 1, y: 0 }}
+            exit={{ scale: 0, y: "-80%" }}
+            transition={transition1}
+            className="flex-1 pt-36 pb-14 lg:pt-0 lg:w-auto z-10 flex flex-col justify-center items-center lg:items-start"
+          >
+            <h1 className="h1">About me</h1>
+            <p className="mb-12 max-w-sm">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+              <b> Exercitationem, veritatis.</b> Veritatis illum aut,
+              reprehenderit sed dolorem dolore.
+              <br />
+              <br />
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit.
+              Recusandae, iure! Ullam, dolore? Eligendi, quam mollitia.
+            </p>
+            <Link to={"/portfolio"} className="btn">
+              View my work
+            </Link>
+          </motion.div>
+        </div>
+      </div>
+    </motion.section>
+  );
+};
+
+export default About;
